933d44ffc97328f085f5d6cb583d36b4b9213387
Edwin Jorge <ed.jorge1122@gmail.com>
2022-05-05 23:35:03 -0500

    EDIT: about section update and skills logos relocated

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,21 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { images } from '../../constants'
 import { motion } from 'framer-motion'
 import { urlFor, client } from '../../client'
 import './Styles.scss'
 
 export const About = () => {
-  const scaleVariants = {
-    whileInView: {
-      scale: [0, 1],
-      opacity: [0, 1],
-      transition: {
-        duration: 1,
-        ease: 'easeInOut'
-      }
-    }
-  }
-
   const [abouts, setAbouts] = useState([])
 
   useEffect(() => {
@@ -27,47 +15,27 @@ export const About = () => {
   return (
     <div className='app__about app__flex'>
       <h2 className='head-text'>
-        My <span>efficient work in</span>
+        From <span>My efficient work</span> in
       </h2>
       <div className='app__profiles'>
-        {
-          abouts.map((about, index) => (
-            <motion.article
-              key={about.title + index}
-              whileInView={{ opacity: 1 }}
-              whileHover={{ scale: 1.055 }}
-              transition={{ duration: 0.5, type: 'tween' }}
-              className='app__profile-item'
-            >
-              <img src={urlFor(about.imgUrl)} alt={about.title} />
-              <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
-              <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
-            </motion.article>
-          ))
-        }
-      </div>
-      <h2 className='head-text'>
-        gives you <span>results and effective <u>work</u> </span>
-      </h2>
-      <motion.div
-        variant={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className='app__header-circles'
-      >
-        {[
-          images.redux,
-          images.react,
-          images.node,
-          images.sass,
-          images.html,
-          images.css,
-          images.git
-        ].map((circle, index) => (
-          <div className='circle-cmp app__flex' key={`circle-${index}`}>
-            <img src={circle} alt='circle' />
-          </div>
+        {abouts.map((about, index) => (
+          <motion.article
+            key={about.title + index}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.055 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.article>
         ))}
-      </motion.div>
+      </div>
     </div>
   )
 }
--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { images } from '../../constants'
+import { motion } from 'framer-motion'
 import './Styles.scss'
 
 export const Skills = () => {
+  const scaleVariants = {
+    whileInView: {
+      scale: [0, 1],
+      opacity: [0, 1],
+      transition: {
+        duration: 1,
+        ease: 'easeInOut'
+      }
+    }
+  }
   return (
-    <div>Skills</div>
+    <>
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header-circles'
+      >
+        {[
+          images.redux,
+          images.react,
+          images.node,
+          images.sass,
+          images.html,
+          images.css,
+          images.git
+        ].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt='circle' />
+          </div>
+        ))}
+      </motion.div>
+    </>
   )
 }
